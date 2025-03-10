c47bf6649e7e8925ab3e2ab285a4f9431c2deea8
dali-benothmen <mohamedalibenothmen1@gmail.com>
2025-03-10 23:41:50 +0100

    Set up exports and create main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hookmaster...")
+export { createHookManager } from "./core"
+export { createScopedHookManager } from "./scopedHooks"
+export { createPriorityHookManager, PriorityHookManager } from "./priorityHooks"
+export { createParallelHookManager, ParallelHookManager } from "./parallelHooks"
+
+export { HookManager, HookFunction } from "./types"
+
+export { HookError, HookNotFoundError, DuplicateHookError } from "./errors"
+
+import { createHookManager } from "./core"
+import { createScopedHookManager } from "./scopedHooks"
+import { createPriorityHookManager } from "./priorityHooks"
+import { createParallelHookManager } from "./parallelHooks"
+import { HookError, HookNotFoundError, DuplicateHookError } from "./errors"
+
+export default {
+    createHookManager,
+    createScopedHookManager,
+    createPriorityHookManager,
+    createParallelHookManager,
+    HookError,
+    HookNotFoundError,
+    DuplicateHookError,
+}
